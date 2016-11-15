31965273bad3ba5e70522f896dae878ed5ebb6fd
Javier Mera <Javier@JaviLapchap.(none)>
2016-11-15 09:28:39 -0600

    Added fragment forecast daily layout, and started working on fragment code.

--- app/src/main/java/teamtreehouse/com/stormy/fragments/FragmentDaily.java
+++ app/src/main/java/teamtreehouse/com/stormy/fragments/FragmentDaily.java
@@ -1,9 +1,24 @@
 package teamtreehouse.com.stormy.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import teamtreehouse.com.stormy.R;
+
 /**
  * Created by Javi on 11/14/2016.
  */
 public class FragmentDaily extends ForecastFragmentBase {
+
+    public static FragmentDaily newInstance() {
+
+        FragmentDaily fragment = new FragmentDaily();
+        return fragment;
+    }
 
     @Override
     public String getTitle() {
@@ -11,10 +26,14 @@ public class FragmentDaily extends ForecastFragmentBase {
         return "Daily";
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-    public static FragmentDaily newInstance() {
+        View view = inflater.inflate(R.layout.fragment_forecast_daily, container, false);
 
-        FragmentDaily fragment = new FragmentDaily();
-        return fragment;
+        ButterKnife.bind(this, view);
+
+        return view;
     }
 }
